6b7666074a6282f70ac2ec02ba529261a65e1ff3
Raphael Bernhart <mail@raphaelbernhart.at>
2024-10-29 15:36:01 +0100

    🐛 Fix sorting

--- src/helpers/sort.ts
+++ src/helpers/sort.ts
@@ -3,6 +3,7 @@ export const sortBasedOnDate = (
     desc = true,
 ): Array<any> => {
     const clonedTableData = JSON.parse(JSON.stringify(tableData));
+
     clonedTableData.sort(
         (routeA: Record<string, any>, routeB: Record<string, any>) => {
             const routeADate = new Date(routeA.Byggedato).getTime();
@@ -14,5 +15,57 @@ export const sortBasedOnDate = (
         },
     );
 
+    return clonedTableData;
+};
+
+export const sortBoltLine = (
+    tableData: Array<any>,
+    sortEntryKey: string,
+    desc = true,
+): Array<any> => {
+    const clonedTableData = JSON.parse(JSON.stringify(tableData));
+
+    clonedTableData.sort(
+        (rowA: Record<string, any>, rowB: Record<string, any>) => {
+            const aString = `${rowA[sortEntryKey]}`.replace('AB', '');
+            const bString = `${rowB[sortEntryKey]}`.replace('AB', '');
+
+            return desc === true
+                ? aString.localeCompare(bString, undefined, {
+                      numeric: true,
+                      sensitivity: 'base',
+                  })
+                : bString.localeCompare(aString, undefined, {
+                      numeric: true,
+                      sensitivity: 'base',
+                  });
+        },
+    );
+    return clonedTableData;
+};
+
+export const sortDefault = (
+    tableData: Array<any>,
+    sortEntryKey: string,
+    desc = true,
+): Array<any> => {
+    const clonedTableData = JSON.parse(JSON.stringify(tableData));
+
+    clonedTableData.sort(
+        (rowA: Record<string, any>, rowB: Record<string, any>) => {
+            const aString = `${rowA[sortEntryKey]}`;
+            const bString = `${rowB[sortEntryKey]}`;
+
+            return desc === true
+                ? aString.localeCompare(bString, undefined, {
+                      numeric: true,
+                      sensitivity: 'base',
+                  })
+                : bString.localeCompare(aString, undefined, {
+                      numeric: true,
+                      sensitivity: 'base',
+                  });
+        },
+    );
     return clonedTableData;
 };
